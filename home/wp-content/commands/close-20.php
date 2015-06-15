18c98acdf090c4c4fa832624b32a4f2be15b5ece
omrib <omrib@sears.co.il>
2015-06-15 17:42:19 +0300

    add message after closing dood

--- home/wp-content/commands/close-20.php
+++ home/wp-content/commands/close-20.php
@@ -1,6 +1,6 @@
 <?php
-exec("sh /var/www/html/home/wp-content/commands/close_all_relay20.sh");
 echo '<textarea style="color:#FF0000;" name="message">Turning off the DOOD</textarea>';
+exec("sh /var/www/html/home/wp-content/commands/close_all_relay20.sh");
 sleep(2);
 header('Location:' . $_SERVER['HTTP_REFERER']);
 test
