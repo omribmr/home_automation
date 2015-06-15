5d77d6c7b18c0a39dbe4b86c7f0b64855b584f54
omrib <omrib@sears.co.il>
2015-06-15 17:29:11 +0300

    add message after closing dood

--- home/wp-content/commands/close-20.php
+++ home/wp-content/commands/close-20.php
@@ -1,5 +1,8 @@
 <?php
 exec("sh /var/www/html/home/wp-content/commands/close_all_relay20.sh");
+$str = "Turning off the DOOD";
+echo $str;
+sleep(2);
 header('Location:' . $_SERVER['HTTP_REFERER']);
 test
 ?>
