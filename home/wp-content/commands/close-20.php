0b54252f1bb2a891ccc67105dbc2f9be902d4071
omrib <omrib@sears.co.il>
2015-06-15 18:17:55 +0300

    add message after closing dood by moving to new page

--- home/wp-content/commands/close-20.php
+++ home/wp-content/commands/close-20.php
@@ -1,6 +1,6 @@
 <?php
-echo '<textarea style="color:#FF0000;" name="message">Turning off the DOOD</textarea>';
 exec("sh /var/www/html/home/wp-content/commands/close_all_relay20.sh");
+header('Location: hhttp://home.omri-bm.com:81/?page_id=31');
 sleep(2);
 header('Location:' . $_SERVER['HTTP_REFERER']);
 test
