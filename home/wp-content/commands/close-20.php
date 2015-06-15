11f8016bdc8f3ed4c758e21c4edf5fe9eb6c72b6
omrib <omrib@sears.co.il>
2015-06-15 19:01:42 +0300

    add message after closing dood by moving to new page

--- home/wp-content/commands/close-20.php
+++ home/wp-content/commands/close-20.php
@@ -1,6 +1,6 @@
 <?php
 exec("sh /var/www/html/home/wp-content/commands/close_all_relay20.sh");
-header('Location: http://home.omri-bm.com:81/?page_id=31');
+header('Location: /?page_id=31');
 sleep(2);
 header('Location:' . $_SERVER['HTTP_REFERER']);
 test
