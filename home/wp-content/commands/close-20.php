64a9b2521755f0f114b08dce51621648e4c84751
omrib <omrib@sears.co.il>
2015-06-15 19:21:54 +0300

    remove message from close

--- home/wp-content/commands/close-20.php
+++ home/wp-content/commands/close-20.php
@@ -1,7 +1,6 @@
 <?php
 exec("sh /var/www/html/home/wp-content/commands/close_all_relay20.sh");
-echo '<script type="text/javascript">alert("הדוד כבוי");window.history.go(-1);</script>';
-sleep(2);
+sleep(3);
 header('Location:' . $_SERVER['HTTP_REFERER']);
 test
 ?>
