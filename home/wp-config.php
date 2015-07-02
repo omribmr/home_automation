b0fcff5c5c9b3da6ca8753a9c5647dae28d0f847
omrib <omrib@sears.co.il>
2015-07-02 19:12:10 +0300

    removing details from config

--- home/wp-config.php
+++ home/wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', '*********');
+define('DB_NAME', 'home');
 
 /** MySQL database username */
-define('DB_USER', '*******');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '*********');
+define('DB_PASSWORD', 'Omri1982');
 
 /** MySQL hostname */
-define('DB_HOST', '**********');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
