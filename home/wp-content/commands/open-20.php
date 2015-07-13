128562a3c4de55e6bfa524df0b59836387aeb5de
omrib <omrib@sears.co.il>
2015-07-13 13:21:33 +0300

    fix url

--- home/wp-content/commands/open-20.php
+++ home/wp-content/commands/open-20.php
@@ -1,5 +1,5 @@
 <?php
 exec("sh /var/www/html/home/wp-content/commands/open_all_relay20.sh");
-header( "refresh:1;url=http://home.omri-bm.com/wp-content/messages/dud-is-on.php" );
+header( 'Location:' url="http://home.omri-bm.com/wp-content/messages/dud-is-on.php" );
 header('Location:' . $_SERVER['HTTP_REFERER']);
 ?>
