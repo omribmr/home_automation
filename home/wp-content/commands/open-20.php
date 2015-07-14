48c77af8c6151022ba1d1c7697aaee23807e42b0
omri <omri.bmr@gmail.com>
2015-07-14 23:58:44 +0300

    fix message

--- home/wp-content/commands/open-20.php
+++ home/wp-content/commands/open-20.php
@@ -1,7 +1,7 @@
 <?php
 exec("sh /var/www/html/home/wp-content/commands/open_all_relay20.sh");
-header('Location:' . $_SERVER['HTTP_REFERER']);
-echo "DUD IS ON";
-sleep (3);
-//header("location: /wp-content/messages/dud-is-on.php");
+//header('Location:' . $_SERVER['HTTP_REFERER']);
+//echo "DUD IS ON";
+//sleep (3);
+header("location: /wp-content/messages/dud-is-on.php");
 ?>
