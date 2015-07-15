7030e4ce3d83651059c600af4b624875af88a5b6
omri <omri.bmr@gmail.com>
2015-07-15 23:19:09 +0300

    fix turn on message

--- home/wp-content/commands/open-20.php
+++ home/wp-content/commands/open-20.php
@@ -2,5 +2,5 @@
 exec("sh /var/www/html/home/wp-content/commands/open_all_relay20.sh");
 //header('Location:' . $_SERVER['HTTP_REFERER']);
 //sleep (3);
-header("location: http://home.omri-bm.com:81/wp-content/messages/dud-is-on.php");
+header("location: http://home.omri-bm.com:81/?page_id=43");
 ?>
