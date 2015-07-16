e4f322c556c14af89a7e83ec4f0111218459e26a
omri <omri.bmr@gmail.com>
2015-07-16 23:09:54 +0300

    add message to 1h and 15m

--- home/wp-content/commands/open-15min-20.php
+++ home/wp-content/commands/open-15min-20.php
@@ -1,4 +1,4 @@
 <?php
 exec("sh /var/www/html/home/wp-content/commands/open_all_15min_relay20.sh");
-header('Location:' . $_SERVER['HTTP_REFERER']);
+header("location: http://home.omri-bm.com:81/?page_id=43");
 ?>
