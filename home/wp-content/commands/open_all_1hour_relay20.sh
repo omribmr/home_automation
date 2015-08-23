c1bd9a86e3a351ac0506059cf7c31a90718645b4
omri <omri.bmr@gmail.com>
2015-08-23 21:20:33 +0300

    fix open  1h close sh file

--- home/wp-content/commands/open_all_1hour_relay20.sh
+++ home/wp-content/commands/open_all_1hour_relay20.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 sh /var/www/html/home/wp-content/commands/open_all_relay20.sh
-at -f /var/www/html/home/wp-content/commands/close_all_relay20.sh now + 1 hour
+at -f sh /var/www/html/home/wp-content/commands/close_all_relay20.sh now + 1 hour
 exit
