e9b80cfe3b90ff3f2ac3a8c28b19490d382be27b
omri <omri.bmr@gmail.com>
2015-08-31 00:31:56 +0300

    fix

--- home/wp-content/commands/open-20.php
+++ home/wp-content/commands/open-20.php
@@ -1,6 +1,5 @@
 <?php
 exec("sh /var/www/html/home/wp-content/commands/open_all_relay20.sh");
 //header('Location:' . $_SERVER['HTTP_REFERER']);
-//sleep (3);
 header("location: http://home.omri-bm.com:81/?page_id=43");
 ?>
